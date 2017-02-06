7e14489864151059cbb4f21a6a64fb67a272b746
Prashant Bhat <prashantbhat84@gmail.com>
2017-02-06 17:28:25 +0530

    new commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var middleware={
 };
 app.use(middleware.logger);
 app.get('/',middleware.requireauthentication, function (req,res) {
-res.send('hello express');
+res.send('hello express js');
 
 });
 
